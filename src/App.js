1ad810492cb6df20a00813c6ecbff3296ae23175
TheAlexGET <sucevanalex06@gmail.com>
2023-10-18 12:21:16 +0200

    added comments

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React from "react";
-import About from "./pages/About";
-import Posts from "./pages/Posts";
-import './styles/css/style.css'
+import { BrowserRouter } from "react-router-dom";
+import AppRouter from "./components/AppRouter";
 import Navbar from "./components/UI/Navbar/Navbar";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Error from "./pages/Error";
+import './styles/css/style.css';
 
 function App() {
   return (
     <BrowserRouter>
     <Navbar/>
-    <Routes>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/posts" element={<Posts/>} />
-      <Route path="/error" element={<Error/>}></Route>
-      <Route path="*" element={<Navigate to={'/error'}/>}></Route>
-    </Routes>
+    <AppRouter></AppRouter>
     </BrowserRouter>
   );
 }
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import MyButton from './UI/button/MyButton';
+import React from "react";
+import MyButton from "./UI/button/MyButton";
+import { useNavigate } from "react-router-dom";
 
-const PostItem = ({number, post, remove}) => {
-  const removePost = (e) => {
-    e.preventDefault()
-    remove(post)
-  }
+const PostItem = ({ number, post, remove }) => {
+  const navigate = useNavigate()
   return (
-      <div className='post'>
-        <div className='post__content'>
-          <strong>{number}. {post.title}</strong>
-          <div>
-          {post.body}
-          </div>
-        </div>
-        <div className='post__btns'>
-            <MyButton onClick={removePost}>Delete</MyButton>
-          </div>
+    <div className="post">
+      <div className="post__content">
+        <strong>
+          {number}. {post.title}
+        </strong>
+        <div>{post.body}</div>
       </div>
+      <div className="post__btns">
+        <MyButton onClick={() => navigate(`/posts/${number}`)}>Open</MyButton>
+        <MyButton onClick={() => remove(post)}>Delete</MyButton>
+      </div>
+    </div>
   );
-}
+};
 
 export default PostItem;
